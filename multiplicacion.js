e66b1db4e33bf06d36ddf461aaf77ac5c19f6928
kevin <martinezmesa79@gmail.com>
2023-11-07 16:49:58 -0500

    Agregados archivos de resta y multiplicación

---
+++ multiplicacion.js
@@ -0,0 +1,4 @@
+var numero1 = parseFloat(prompt("Ingrese el primer número:"));
+var numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+var multiplicacion = numero1 * numero2;
+console.log("La multiplicación de los dos números es: " + multiplicacion);
---
+++ resta.js
@@ -0,0 +1,4 @@
+var numero1 = parseFloat(prompt("Ingrese el primer número:"));
+var numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+var resta = numero1 - numero2;
+console.log("La resta de los dos números es: " + resta);
