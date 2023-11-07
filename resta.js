62e02a849c787ce99449b0ee5795b94ceb45f4f5
kevin <martinezmesa79@gmail.com>
2023-11-07 17:14:28 -0500

    Agregadas operaciones de suma, resta y multiplicación a la calculadora

--- resta.js
+++
@@ -1,4 +0,0 @@
-var numero1 = parseFloat(prompt("Ingrese el primer número:"));
-var numero2 = parseFloat(prompt("Ingrese el segundo número:"));
-var resta = numero1 - numero2;
-console.log("La resta de los dos números es: " + resta);
--- suma.js
+++
@@ -1,4 +0,0 @@
-var numero1 = parseFloat(prompt("Ingrese el primer número:"));
-var numero2 = parseFloat(prompt("Ingrese el segundo número:"));
-var suma = numero1 + numero2;
-console.log("La suma de los dos números es: " + suma);
